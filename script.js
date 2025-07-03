e6cefac1d3c7c8bf02231ebab6f524c0cb07ef51
Henrique Susin <henriquesusin@outlook.com.br>
2025-07-03 09:24:23 -0300

    Falta só implementar bandeiras e vitória

--- script.js
+++ script.js
@@ -52,13 +52,13 @@ function espalhar(linhas, colunas, dificuldade)
 
 	switch (dificuldade) {
 		case "facil":
-			porcentagem = 0.4
+			porcentagem = 0.2
 			break
 		case "medio":
-			porcentagem = 0.6
+			porcentagem = 0.4
 			break
 		case "dificil":
-			porcentagem = 0.8
+			porcentagem = 0.6
 	}
 
 	quantas = Math.round(linhas * colunas * porcentagem)
@@ -88,45 +88,50 @@ function contagem(campo, bombas, linhas, colunas)
 
 	for (let i = 0; i < linhas; i++) {
 		for (let j = 0; j < colunas; j++) {
+			const sup = i - 1
+			const inf = i + 1
+			const esq = j - 1
+			const drt = j + 1
+
 			if (bombas[i][j] != 1) {
 				// superior esquerdo
-				if (i - 1 >= 0 && j - 1 >= 0)
-					if (bombas[i - 1][j - 1])
+				if (sup >= 0 && esq >= 0)
+					if (bombas[sup][esq])
 						contagem++
 
 				// superior
-				if (i - 1 >= 0)
-					if (bombas[i - 1][j])
+				if (sup >= 0)
+					if (bombas[sup][j])
 						contagem++
 
 				// superior direito
-				if (i - 1 >= 0 && j + 1 < colunas)
-					if (bombas[i - 1][j + 1])
+				if (sup >= 0 && j + 1 < colunas)
+					if (bombas[sup][drt])
 						contagem++
 
 				// esquerda
-				if (j - 1 >= 0)
-					if (bombas[i][j - 1])
+				if (esq >= 0)
+					if (bombas[i][esq])
 						contagem++
 
 				// direita
-				if (j + 1 < colunas)
-					if (bombas[i][j + 1])
+				if (drt < colunas)
+					if (bombas[i][drt])
 						contagem++
 
 				// inferior esquerdo
-				if (i + 1 < linhas && j - 1 >= 0)
-					if (bombas[i + 1][j - 1])
+				if (inf < linhas && esq >= 0)
+					if (bombas[inf][esq])
 						contagem++
 
 				// inferior
-				if (i + 1 < linhas)
-					if (bombas[i + 1][j])
+				if (inf < linhas)
+					if (bombas[inf][j])
 						contagem++
 
 				// inferior direito
-				if (i + 1 < linhas && j + 1 < colunas)
-					if (bombas[i + 1][j + 1])
+				if (inf < linhas && drt < colunas)
+					if (bombas[inf][drt])
 						contagem++
 
 				if (contagem != 0)
@@ -142,72 +147,84 @@ function contagem(campo, bombas, linhas, colunas)
 	return numeros
 }
 
-function abrir(celula, linha, coluna) {
-	celula.className = "aberto"
-	celula.innerText = numeros[linha][coluna]
+function redefinir(celula)
+{
 	celula.removeAttribute("onmousedown")
 	celula.removeAttribute("onmouseup")
 	celula.removeAttribute("onclick")
 	celula.removeAttribute("oncontextmenu")
 }
 
+function abrir(celula, linha, coluna) {
+	celula.className = "aberto"
+	celula.innerText = numeros[linha][coluna]
+	redefinir(celula)
+}
+
 function expandir(celula, linha, coluna)
 {
+	const sup = linha - 1
+	const inf = linha + 1
+	const esq = coluna - 1
+	const drt = coluna + 1
+
+	abrir(celula, linha, coluna)
+
 	// superior esquerdo
-	if (linha - 1 >= 0 && coluna - 1 >= 0)
-		if (numeros[linha - 1][coluna - 1])
-			abrir(campo[linha - 1][coluna - 1], linha - 1, coluna - 1)
-		else
-			expandir(campo[linha - 1][coluna - 1], linha - 1, coluna - 1)
+	if (sup >= 0 && esq >= 0)
+		if (numeros[sup][esq])
+			abrir(campo[sup][esq], sup, esq)
+		else if (campo[sup][esq].className != "aberto")
+			expandir(campo[sup][esq], sup, esq)
 
 	// superior
-	if (linha - 1 >= 0)
-		if (numeros[linha - 1][coluna])
-			abrir(campo[linha - 1][coluna], linha - 1, coluna)
-		else
-			expandir(campo[linha - 1][coluna], linha - 1, coluna)
+	if (sup >= 0)
+		if (numeros[sup][coluna])
+			abrir(campo[sup][coluna], sup, coluna)
+		else if (campo[sup][coluna].className != "aberto")
+			expandir(campo[sup][coluna], sup, coluna)
 
 	// superior direito
-	if (linha - 1 >= 0 && coluna + 1 < colunas)
-		if (numeros[linha - 1][coluna + 1])
-			abrir(campo[linha - 1][coluna + 1], linha - 1, coluna + 1)
-		else
-			expandir(campo[linha - 1][coluna + 1], linha - 1, coluna + 1)
-/*
+	if (sup >= 0 && drt < colunas)
+		if (numeros[sup][drt])
+			abrir(campo[sup][drt], sup, drt)
+		else if (campo[sup][drt].className != "aberto")
+			expandir(campo[sup][drt], sup, drt)
+
 	// esquerda
-	if (coluna - 1 >= 0)
-		if (numeros[linha][coluna - 1])
-			abrir(campo, , )
-		else
-			expandir(campo, , )
+	if (esq >= 0)
+		if (numeros[linha][esq])
+			abrir(campo[linha][esq], linha, esq)
+		else if (campo[linha][esq].className != "aberto")
+			expandir(campo[linha][esq], linha, esq)
 
 	// direita
-	if (coluna + 1 < colunas)
-		if (numeros[linha][coluna + 1])
-			abrir(campo, , )
-		else
-			expandir(campo, , )
+	if (drt < colunas)
+		if (numeros[linha][drt])
+			abrir(campo[linha][drt], linha, drt)
+		else if (campo[linha][drt].className != "aberto")
+			expandir(campo[linha][drt], linha, drt)
 
 	// inferior esquerdo
-	if (linha + 1 < linhas && coluna - 1 >= 0)
-		if (numeros[linha + 1][coluna - 1])
-			abrir(campo, , )
-		else
-			expandir(campo, , )
+	if (inf < linhas && esq >= 0)
+		if (numeros[inf][esq])
+			abrir(campo[inf][esq], inf, esq)
+		else if (campo[inf][esq].className != "aberto")
+			expandir(campo[inf][esq], inf, esq)
 
 	// inferior
-	if (linha + 1 < linhas)
-		if (numeros[linha + 1][coluna])
-			abrir(campo, , )
-		else
-			expandir(campo, , )
+	if (inf < linhas)
+		if (numeros[inf][coluna])
+			abrir(campo[inf][coluna], inf, coluna)
+		else if (campo[inf][coluna].className != "aberto")
+			expandir(campo[inf][coluna], inf, coluna)
 
 	// inferior direito
-	if (linha + 1 < linhas && coluna + 1 < colunas)
-		abrir(campo, , )
-		else
-			expandir(campo, , )
-*/
+	if (inf < linhas && drt < colunas)
+		if (numeros[inf][drt])
+			abrir(campo[inf][drt], inf, drt)
+		else if (campo[inf][drt].className != "aberto")
+			expandir(campo[inf][drt], inf, drt)
 }
 
 function verificar(celula)
@@ -216,19 +233,23 @@ function verificar(celula)
 
 	const pos = celula.id.split("_")
 
-	const linha = pos[0]
-	const coluna = pos[1]
+	const linha = Number(pos[0])
+	const coluna = Number(pos[1])
 
 	for (let i = 0; i < linhas; i++) {
 		for (let j = 0; j < colunas; j++) {
 			if (bombas[linha][coluna]) {
 				for (let i = 0; i < linhas; i++)
-					for (let j = 0; j < colunas; j++)
-						if (bombas[i][j])
+					for (let j = 0; j < colunas; j++) {
+						if (bombas[i][j]) {
 							campo[i][j].innerText = "💣"
+							campo[i][j].style.cursor = "default"
+						} else
+							abrir(campo[i][j], i, j)
+						redefinir(campo[i][j])
+					}
 				celula.innerText = "💥"
 			} else if (!numeros[linha][coluna]) {
-				abrir(celula, linha, coluna)
 				expandir(celula, linha, coluna)
 			} else {
 				abrir(celula, linha, coluna)
@@ -242,12 +263,14 @@ function bandeira(celula)
 
 }
 
-function iniciar(dificuldade, linhas, colunas)
+function iniciar()
 {
+	const antigo = document.getElementById("campo")
 
-}
+	antigo.getElementsByTagName("tbody")[0].remove()
 
-onload = () => {
+	antigo.appendChild(document.createElement("tbody"))
+
 	linhas = document.getElementById("linhas").value
 	colunas = document.getElementById("colunas").value
 	dificuldade = document.getElementById("dificuldade").value
@@ -255,4 +278,8 @@ onload = () => {
 	campo = criar(linhas, colunas)
 	bombas = espalhar(linhas, colunas, dificuldade)
 	numeros = contagem(campo, bombas, linhas, colunas)
+}
+
+onload = () => {
+	iniciar()
 }
